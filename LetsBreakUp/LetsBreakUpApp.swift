3719190ba3b7946248c6335b1fe24c896f306333
Hongpil Hwang <hwanghongpil@gmail.com>
2024-03-06 20:34:53 +0900

    Initial Commit

---
+++ LetsBreakUp/LetsBreakUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsBreakUpApp.swift
+//  LetsBreakUp
+//
+//  Created by 황홍필 on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct LetsBreakUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
